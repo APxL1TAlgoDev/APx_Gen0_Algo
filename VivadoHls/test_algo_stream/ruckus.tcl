5fd3b625e546acaf3e9dae16668d728703f1aff2
Ales Svetek <ales.svetek@cern.ch>
2019-01-07 07:02:01 -0600

    Cleanup

--- VivadoHls/test_algo_stream/ruckus.tcl
+++ VivadoHls/test_algo_stream/ruckus.tcl
@@ -5,7 +5,4 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 loadSource -path "$::DIR_PATH/rtl/algo_top_wrapper.vhd"
 loadSource -path "$::DIR_PATH/rtl/algo_pkg.vhd"
 
-#loadSource -path "$::DIR_PATH/vivado_hls/ip/algo_top.dcp"
-#loadSource -path "$::DIR_PATH/vivado_hls/ip/algo_top_stub.vhd"
-
 loadSource -dir "$::DIR_PATH/vivado_hls/proj/solution1/impl/ip/hdl/vhdl/"
